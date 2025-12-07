990edce9e39e0e0320fe2f91cab9cfd411e5e769
code-ananya <ananyapandey22222@gmail.com>
2025-12-07 13:15:54 +0530

    stream user creation

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 
 export const inngest = new Inngest({ id: "hire-wave" });
@@ -40,6 +41,12 @@ if (!userEmail) {
 
     await User.create(newUser);
 
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image:newUser.profileImage,
+    });
+
     
   }
 );
@@ -53,7 +60,8 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    
+    await deleteStreamUser(id.toString());
+
   }
 );
 
